34fcb0221f19f8c165f5ea7e0da23c36c4b9fe21
Lenovo <Lenovo@DESKTOP-HAFF92D>
2024-07-08 22:25:29 +0530

    Add project files.

---
+++ Weblibrary/Webmodel.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Weblibrary
+{
+    public class Webmodel
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+    }
+}
